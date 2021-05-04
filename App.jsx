ba4f7adffe6747125dc061b11b5db66b8b0d67d6
gad <studio360r@gmail.com>
2021-05-04 13:58:05 +0300

    gad

--- App.jsx
+++ App.jsx
@@ -5,8 +5,6 @@ const { Route, Switch, Link } = ReactRouterDOM
 import { Home } from './pages/home.jsx'
 import { AppHeader } from './cmps/AppHeader.jsx'
 import { AppFooter } from './cmps/AppFooter.jsx'
-
-
 import { BookApp } from './apps/Books/BookApp.jsx'
 import { KeepApp } from './apps/Keep/KeepApp.jsx'
 import { MailApp } from './apps/Mail/MailApp.jsx'
@@ -19,10 +17,11 @@ export function App() {
             <AppHeader />
             <main>
                 <Switch>
-                   <Route component={BookApp} path="/BookApp" />
-                   <Route component={KeepApp} path="/KeepApp" />
-                   <Route component={MailApp} path="/MailApp" />
-                   <Route component={Home} path="/" />
+                    <Route component={MailDetails} path="/MailApp/:mailId" />
+                    <Route component={BookApp} path="/BookApp" />
+                    <Route component={KeepApp} path="/KeepApp" />
+                    <Route component={MailApp} path="/MailApp" />
+                    <Route component={Home} path="/" />
                 </Switch>
             </main>
             <AppFooter />
