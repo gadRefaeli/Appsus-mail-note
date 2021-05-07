765155f0ada387a5fa69f5270c9768f17582af9b
gad <studio360r@gmail.com>
2021-05-07 17:55:42 +0300

    new

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 import { UserMsg } from '../cmps/UserMsg.jsx'
->>>>>>> ab961a3b5b7c54fd2f83867bef282dc0c9f14504
 
 export function AppHeader() {
     const { Link } = ReactRouterDOM;
