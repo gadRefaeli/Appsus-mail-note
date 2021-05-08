7be1bbfeeb85ef3ac03d31a9db21b9c4994438f7
gad <studio360r@gmail.com>
2021-05-08 21:20:47 +0300

    a

--- apps/Mail/pages/MailApp.jsx
+++ apps/Mail/pages/MailApp.jsx
@@ -110,9 +110,13 @@ export class MailApp extends React.Component {
                  <div className="mail-side-bar-nav">
                 <h1>Mailbox <span className={sideBarClass} onClick={this.taggleSideBar}><img src="../../../img/compose-02.png" width="30"></img> </span></h1> 
                 
+
+                
+
+
                 {(this.state.sideBar)&&
                 <div className="side-bar-phone">
-                <Link to="/MailApp/compose"><button className="compose-btn"><img src="../../../img/compose-01.png" width="30"></img><span>Compose</span>  </button> </Link>
+                <Link to="/MailApp/compose"><button className="compose-btn"><img src="./img/compose-01.png" width="30"></img><span>Compose</span>  </button> </Link>
                     <MailFilter onSetFilter={this.onSetFilter} getMailCount={this.getMailCount}/>
                     <p> Unread Mails: ({this.state.unreadMailCount})/( {this.state.unreadMailCount+this.state.readMailCount})</p>
                     <div className="bar-unread"> <div className="bar-unread-inner" style={divStyle}></div></div>
